4233e8dfd21c0cf4004b04521382f80b0f043fcc
Adekunle Folorunsho <adekunle.folorunsho@bodadmin.com>
2022-08-12 20:48:15 +0100

    adjust validity check #45

--- src/rateLimiting.ts
+++ src/rateLimiting.ts
@@ -35,7 +35,7 @@ export class WsThrottlerGuard extends ThrottlerGuard {
     else if (ttls.length+1 == limit) {
       await this.storageService.addRecord(ip, ttl); //add the final
       var updated= await this.storageService.getRecord(ip);
-      Tracker.set(ip, updated[limit-1]+ 1200) //give a tolerance of 2 sec;
+      Tracker.set(ip, updated[limit-1]+ 1500) //give a tolerance of 2 sec;
     }
     else{
         console.log(ttls);
